66d49416ce097a8c7124dd0a9a70b12ac4cf413c
CyrusJavan <cyrus.javan@sjsu.edu>
2019-11-20 19:26:41 -0800

    redesign

--- static/js/app.jsx
+++ static/js/app.jsx
@@ -1,24 +1,46 @@
 class App extends React.Component {
   render() {
-    return (<Home />);
+    return (
+      <div>
+        <Home />
+      </div>
+      );
   }
 }
 
 class Home extends React.Component {
   render() {
     return (
-      <div id="jumbotron-title" className="container jumbotron">
+      <div className="container">
         <div className="row">
-          <h1>Cyrus Javan</h1>
-        </div>
-        <div className="row">
-          <h3>Hello. I do software development for a living.</h3>
-        </div>
-        <div className="row">
-          <h3>I currently work at BetterHelp, the world's largest online mental health counseling platform.</h3>
-        </div>
-        <div className="row">
-          <h3>Would communicating with me be potentially fruitful for both parties? Yes? Well that's wonderful, feel free to reach out on <a href="https://www.linkedin.com/in/cyrusjavan" rel="noopener noreferrer" target="_none">LinkedIn</a>.</h3>
+          <div className="col-10 text-center pt-5 pl-5">
+            <div >
+
+            <img className="headshot" src="/static/img/headshot.jpg" alt="cyrus javan headshot"></img>
+            </div>
+            <h1 className="my-3">Cyrus Javan</h1>
+            <h2 className="my-3">Software Engineer</h2>
+            <h2 className="my-3">Currently helping build the world's largest online counseling platform at <a href="https://www.betterhelp.com">BetterHelp</a></h2>
+          </div>
+          <div className="col-2 text-center py-5">
+            <p>
+              <a className="social-icon" href="https://github.com/CyrusJavan">
+              <i className="fa fa-github"></i>
+              </a>
+            </p>
+            <p>
+              <a className="social-icon"  href="mailto:javan.cyrus@gmail.com">
+              <i className="fa fa-envelope"></i>
+
+              </a>
+            </p>
+            <p>
+              <a className="social-icon" href="https://www.linkedin.com/in/cyrusjavan">
+              <i className="fa fa-linkedin"></i>
+
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     );
